169d14e2d417e064c51dfcffde21291f99d180b2
Daniil Kalamin <56044849+dakalamin@users.noreply.github.com>
2019-12-05 03:12:49 +0300

    Initial commit

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,10 +6,49 @@ using System.Threading.Tasks;
 
 namespace Lab1
 {
+
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            Console.Title = "Daniil Kalamin -- IU5-34";
+
+            bool test = double.TryParse(args[0], out double A);
+            test &= double.TryParse(args[1], out double B);
+            test &= double.TryParse(args[2], out double C);
+
+            if (!test)
+            {
+                Console.WriteLine("Please enter A, B and C koeffs.");
+                return 1;
+            }
+
+            double discr = B*B - 4*A*C;
+            if (discr > 0)
+            {
+                double res1 = (-B + Math.Sqrt(discr)) / (2*A);
+                double res2 = (-B - Math.Sqrt(discr)) / (2*A);
+
+                Console.Write("x1 = ");
+                Console.WriteLine(res1);
+                Console.Write("x2 = ");
+                Console.WriteLine(res2);
+
+            }
+            else if (discr == 0)
+            {
+                double res = -B/(2*A);
+
+                Console.Write("x = ");
+                Console.WriteLine(res);
+            }
+            else
+            {
+                Console.WriteLine("No real solutions");
+            }
+
+            Console.ReadKey();
+            return 0;
         }
     }
 }
