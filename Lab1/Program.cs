e3e1459f34672f2316be7abdf116f21873b175b9
Daniil Kalamin <56044849+dakalamin@users.noreply.github.com>
2019-12-05 03:24:04 +0300

    added a=0 and b=0 solutions

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -23,28 +23,47 @@ namespace Lab1
                 return 1;
             }
 
-            double discr = B*B - 4*A*C;
-            if (discr > 0)
+            if (A == 0)
             {
-                double res1 = (-B + Math.Sqrt(discr)) / (2*A);
-                double res2 = (-B - Math.Sqrt(discr)) / (2*A);
+                if (B == 0)
+                {
+                    Console.WriteLine("(not actually an equation)");
+                    Console.WriteLine("x - is any real number");
+                }
+                else
+                {
+                    double res = -C / B;
 
-                Console.Write("x1 = ");
-                Console.WriteLine(res1);
-                Console.Write("x2 = ");
-                Console.WriteLine(res2);
-
-            }
-            else if (discr == 0)
-            {
-                double res = -B/(2*A);
-
-                Console.Write("x = ");
-                Console.WriteLine(res);
+                    Console.WriteLine("(not actually a quadratic equation)");
+                    Console.Write("x = ");
+                    Console.WriteLine(res);
+                }
             }
             else
             {
-                Console.WriteLine("No real solutions");
+                double discr = B * B - 4 * A * C;
+                if (discr > 0)
+                {
+                    double res1 = (-B + Math.Sqrt(discr)) / (2 * A);
+                    double res2 = (-B - Math.Sqrt(discr)) / (2 * A);
+
+                    Console.Write("x1 = ");
+                    Console.WriteLine(res1);
+                    Console.Write("x2 = ");
+                    Console.WriteLine(res2);
+
+                }
+                else if (discr == 0)
+                {
+                    double res = -B / (2 * A);
+
+                    Console.Write("x = ");
+                    Console.WriteLine(res);
+                }
+                else
+                {
+                    Console.WriteLine("No real solutions");
+                }
             }
 
             Console.ReadKey();
