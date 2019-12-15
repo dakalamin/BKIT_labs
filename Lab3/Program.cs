a9d2bfb6f32b010b0016eed9446dd4dad38796b5
Daniil Kalamin <56044849+dakalamin@users.noreply.github.com>
2019-12-16 01:49:02 +0300

    added sort events

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -12,11 +12,39 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
-            Rectangle rect = new Rectangle(5, 4);
-            Square  square = new Square(5);
-            Circle  circle = new Circle(5);
+            Rectangle rect = new Rectangle(8, 5);
+            Square  square = new Square(6.5);
+            Circle  circle = new Circle(3);
 
             ArrayList array = new ArrayList();
+            array.Add(rect);
+            array.Add(square);
+            array.Add(circle);
+
+            Console.WriteLine(" -- ARRAYLIST -- ");
+            Console.WriteLine("before comparison:");
+            foreach (var x in array) Console.WriteLine(x);
+            array.Sort();
+
+            Console.WriteLine("\nafter comparison:");
+            foreach (var x in array) Console.WriteLine(x);
+            Console.WriteLine("\n");
+
+
+            List<Shape> list = new List<Shape>();
+            list.Add(square);
+            list.Add(circle);
+            list.Add(rect);
+
+            Console.WriteLine(" -- LIST<> -- ");
+            Console.WriteLine("before comparison:");
+            foreach (var x in list) Console.WriteLine(x);
+            list.Sort();
+
+            Console.WriteLine("\nafter comparison:");
+            foreach (var x in list) Console.WriteLine(x);
+
+            Console.ReadKey();
         }
     }
 }
