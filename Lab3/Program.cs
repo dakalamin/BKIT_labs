e0b1d0b925390f9a25e55065078f94893fd440f9
Daniil Kalamin <56044849+dakalamin@users.noreply.github.com>
2019-12-16 00:44:09 +0300

    Initialized Lab3

---
+++ Lab3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab2;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(5, 4);
+            Square  square = new Square(5);
+            Circle  circle = new Circle(5);
+
+            ArrayList array = new ArrayList();
+        }
+    }
+}
